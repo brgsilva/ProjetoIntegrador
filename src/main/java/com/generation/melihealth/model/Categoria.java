2dfec1179409b154cc1418fb236f8a1fef44db91
williamkolosque <william.maich@mercadolivre.com>
2023-02-02 10:53:50 -0300

    feito o a implementação da classe produto e a conexão com o banco

---
+++ src/main/java/com/generation/melihealth/model/Categoria.java
@@ -0,0 +1,4 @@
+package com.generation.melihealth.model;
+
+public class Categoria {
+}
---
+++ src/main/java/com/generation/melihealth/model/Produto.java
@@ -0,0 +1,108 @@
+package com.generation.melihealth.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = ("tb_produto"))
+public class Produto {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 1000)
+    private String especialidade;
+
+
+    @Size(max = 250)
+    private String descricao;
+
+    @UpdateTimestamp
+    private LocalDate dataInicio;
+
+    @UpdateTimestamp
+    private LocalDate dataFim;
+
+
+    private String foto;
+
+    @NotNull
+    private boolean tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public boolean isTipo() {
+        return tipo;
+    }
+
+    public void setTipo(boolean tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
