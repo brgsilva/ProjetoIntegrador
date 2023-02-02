bb940e22ab8a825697234fbc194e784a5923cfa5
Daniel-Hermel-MELI <daniel.hermel@mercadolivre.com>
2023-02-02 11:15:47 -0300

    Correção da classe Produto.

---
+++ src/main/java/model/Produto.java
@@ -0,0 +1,83 @@
+package model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = ("tb_produtos"))
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 1000)
+    private String especialidade;
+
+    @Size(max = 200)
+    private String descricao;
+
+    @UpdateTimestamp
+    private LocalDate dataInicio;
+
+    @UpdateTimestamp
+    private LocalDate dataFinal;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(LocalDate dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+}
