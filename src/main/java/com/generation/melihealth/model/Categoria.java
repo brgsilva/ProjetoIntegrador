c3a380524e9c4d715a82b3dd597bdb05cccdebe0
Abner <abner.batista@mercadolivre.com>
2023-02-02 13:41:31 -0300

    Feito

---
+++ src/main/java/com/generation/melihealth/model/Categoria.java
@@ -0,0 +1,23 @@
+package com.generation.melihealth.model;
+
+public class Categoria {
+
+    private Long id;
+    private String tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ src/main/java/com/generation/melihealth/model/Produto.java
@@ -0,0 +1,83 @@
+package com.generation.melihealth.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = ("tb_produto"))
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String nome;
+
+    @NotBlank
+    @Size(max = 100)
+    private String especialidade;
+
+    @Size(max = 200)
+    private String descricao;
+
+    @UpdateTimestamp
+    private LocalDate dataInicio;
+
+    @UpdateTimestamp
+    private LocalDate dataFim;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
