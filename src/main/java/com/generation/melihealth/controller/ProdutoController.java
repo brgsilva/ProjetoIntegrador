c12c3ed89999d83bd349101beed202717ccf9555
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-03 11:38:10 -0300

    Ateracao realizado na classe produto, endpoint getById implementado

--- src/main/java/com/generation/melihealth/controller/ProdutoController.java
+++ src/main/java/com/generation/melihealth/controller/ProdutoController.java
@@ -3,11 +3,9 @@ package com.generation.melihealth.controller;
 import com.generation.melihealth.model.Produto;
 import com.generation.melihealth.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,14 @@ public class ProdutoController {
     public ResponseEntity<List<Produto>> getAll(){
         return ResponseEntity.ok().body(produtoRepository.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getById(@PathVariable Long id){
+        return produtoRepository.findById(id)
+                .map(response -> ResponseEntity.ok(response))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+
 
 }
