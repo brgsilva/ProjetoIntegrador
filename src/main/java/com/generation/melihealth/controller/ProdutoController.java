6a5527463572a67295c4e43e9952cebc6013b4f8
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-03 10:46:26 -0300

    Repository, controller e endpoint getAll implementado

---
+++ src/main/java/com/generation/melihealth/controller/ProdutoController.java
@@ -0,0 +1,27 @@
+package com.generation.melihealth.controller;
+
+import com.generation.melihealth.model.Produto;
+import com.generation.melihealth.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> getAll(){
+        return ResponseEntity.ok().body(produtoRepository.findAll());
+    }
+
+}
