156a4c64beeeafd4b9c209d166b26c90be51d363
AlexandreMarcelino <alexandre.msantos@mercadolivre.com>
2023-02-03 14:43:19 -0300

    Endpoint para atualizar produtos implementado

--- src/main/java/com/generation/melihealth/controller/ProdutoController.java
+++ src/main/java/com/generation/melihealth/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package com.generation.melihealth.controller;
 
 import com.generation.melihealth.model.Produto;
 import com.generation.melihealth.repository.ProdutoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,24 @@ public class ProdutoController {
                 .map(response -> ResponseEntity.ok(response))
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
+
+    @GetMapping("/especialidade/{especialidade}")
+    public ResponseEntity<List<Produto>> getByEspecialidade(@PathVariable String especialidade) {
+        return ResponseEntity.ok(produtoRepository.findAllByEspecialidadeContainingIgnoreCase(especialidade));
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+    }
+
+    @PutMapping
+    public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
+        getById(produto.getId());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(produtoRepository.save(produto));
+    }
+
 
 
 
