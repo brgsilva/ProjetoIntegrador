1241a87a43115bd18c686c61893554619ffaf89e
Daniel-Hermel-MELI <daniel.hermel@mercadolivre.com>
2023-02-03 15:24:50 -0300

    Commit - ProdutoController-2

--- src/main/java/com/generation/melihealth/controller/ProdutoController.java
+++ src/main/java/com/generation/melihealth/controller/ProdutoController.java
@@ -4,11 +4,14 @@ import com.generation.melihealth.model.Produto;
 import com.generation.melihealth.repository.ProdutoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -41,9 +44,24 @@ public class ProdutoController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(produtoRepository.save(produto));
-
     }
 
+    @PutMapping
+    public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
+        return produtoRepository.findById(produto.getId())
+                .map(reponse -> ResponseEntity.status(HttpStatus.OK)
+                    .body(produtoRepository.save(produto)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        Optional<Produto> produto = produtoRepository.findById(id);
 
+        if (produto.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        produtoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
