93324a2f47c6a85638ecadfbaba138b52736ed0c
Bruno <bruno.gsilva@mercadolivre.com>
2023-02-03 16:11:38 -0300

    criado EndPoint de getByCategoria, pesquisa por categoria.

--- src/main/java/com/generation/melihealth/controller/CategoriaController.java
+++ src/main/java/com/generation/melihealth/controller/CategoriaController.java
@@ -3,10 +3,9 @@ package com.generation.melihealth.controller;
 import com.generation.melihealth.model.Categoria;
 import com.generation.melihealth.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +17,19 @@ public class CategoriaController {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
-
+    @GetMapping
     public ResponseEntity<List<Categoria>> getAll(){
         return ResponseEntity.ok(categoriaRepository.findAll());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> getById(@PathVariable Long id){
+        return categoriaRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+    @GetMapping("/tipo/{tipo}")
+    public ResponseEntity<List<Categoria>> getByCategoria(@PathVariable String tipo) {
+        return ResponseEntity.ok(categoriaRepository.findAllByTipoContainingIgnoreCase(tipo));
     }
 
 
--- src/main/java/com/generation/melihealth/repository/CategoriaRepository.java
+++ src/main/java/com/generation/melihealth/repository/CategoriaRepository.java
@@ -1,11 +1,15 @@
 package com.generation.melihealth.repository;
 
 import com.generation.melihealth.model.Categoria;
+import com.generation.melihealth.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-
+    public List<Categoria> findAllByTipoContainingIgnoreCase(@Param("tipo") String tipo);
 
 }
