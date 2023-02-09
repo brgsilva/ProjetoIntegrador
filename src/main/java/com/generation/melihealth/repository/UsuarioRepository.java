b1c5fb774e04dce931e5390258d1a84cf2482c2c
Bruno <bruno.gsilva@mercadolivre.com>
2023-02-09 10:04:45 -0300

    criado classe usuario Repository

---
+++ src/main/java/com/generation/melihealth/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.generation.melihealth.repository;
+
+
+import com.generation.melihealth.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    public Optional<Usuario> findAllByUsuario(String usuario);
+
+}
