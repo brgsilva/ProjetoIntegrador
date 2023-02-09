51c3366c7258d809ef12018ba2b742db1d3f54c5
Bruno <bruno.gsilva@mercadolivre.com>
2023-02-09 09:52:51 -0300

    criado classe usuario e usuario login

--- src/main/java/com/generation/melihealth/model/Produto.java
+++ src/main/java/com/generation/melihealth/model/Produto.java
@@ -33,6 +33,11 @@ public class Produto {
     @JsonIgnoreProperties("produto")
     private Categoria categoria;
 
+    @ManyToOne
+    @JsonIgnoreProperties("produto")
+    private Usuario usuario;
+
+
     public Long getId() {
         return id;
     }
@@ -79,5 +84,13 @@ public class Produto {
 
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+
+    }
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
 }
